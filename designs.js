04e3a48fbebb30a1b84bc94db9fde3427b4f0bba
Joseph Martinez <joseph.r.martinez@gmail.com>
2022-02-02 20:53:21 -0700

    Assign input variables

--- designs.js
+++ designs.js
@@ -1,5 +1,9 @@
 // Select color input
+let color = document.querySelector('#colorPicker');
+
 // Select size input
+let grid = document.getElementById('pixelCanvas');
+let picker = document.getElementById('sizePicker');
 
 // When size is submitted by the user, call makeGrid()
 
