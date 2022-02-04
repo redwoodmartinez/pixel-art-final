af6c6e63d5bee4e01ebc420d282a22f8fa487aec
Joseph Martinez <joseph.r.martinez@gmail.com>
2022-02-03 20:52:58 -0700

    Add descriptive comments to functions

--- designs.js
+++ designs.js
@@ -5,9 +5,16 @@ let color = document.querySelector('#colorPicker');
 let grid = document.getElementById('pixelCanvas');
 let picker = document.getElementById('sizePicker');
 
+// Color to be applied to grid changes when color picker selection changes
 picker.addEventListener('submit', makeGrid);
 
 // When size is submitted by the user, call makeGrid()
+// /**
+//   * @description Creates a grid of squares
+//   * @description Size is based on user supplied inputs for height and width
+//   * @param int $row - number of squares representing the height of the grid
+//   * @param int $column - number of squares representing the width of the grid
+// */
 function makeGrid(event) {
   grid.innerHTML = '';
   event.preventDefault();
@@ -20,8 +27,10 @@ function makeGrid(event) {
       addCol.onclick = changeColor;
     }
   }
-};
+}
 
+
+// @description Square color change is applied when user clicks square
 function changeColor() {
   this.style.background = color.value;
 }
